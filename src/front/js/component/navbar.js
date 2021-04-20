1dd0b7b579c1b3a75200cd0fa0bb05b91fdab7ea
jairosantamaria <jairosantamaria@amelearning.net>
2021-04-20 22:43:31 +0000

    navbar-no terminado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,14 +3,41 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-warning">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Por qué QR+
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarText"
+					aria-controls="navbarText"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarText">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">
+								Ingresar
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Acerca de
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Registrarse
+							</a>
+						</li>
+					</ul>
+					<span className="navbar-text">QR+</span>
+				</div>
 			</div>
 		</nav>
 	);

d3ea1448a3df7cd2d468595e2c90f096cfb46615
jairosantamaria <jairosantamaria@amelearning.net>
2021-04-23 16:08:16 +0000

    rev11

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,11 +7,12 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5 imgmain-class">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src="" />
-			</p>
+		<div classNameName="text-center mt-5 imgmain-className">
+			<header className="w3-container w3-red w3-center">
+				<h1 className="w3-margin w3-jumbo">START PAGE</h1>
+				<p className="w3-xlarge">Template by w3.css</p>
+				<button className="w3-button w3-black w3-padding-large w3-large w3-margin-top">Get Started</button>
+			</header>
 		</div>
 	);
 };

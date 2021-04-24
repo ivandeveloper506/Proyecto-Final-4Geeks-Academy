aa8a7c872dca56564432d1ba6bfa8fd5183d2b5f
cbenavides1589 <cbenavides1589@gmail.com>
2021-04-24 22:16:29 +0000

    actualizacion quienes somos

--- src/front/js/pages/about-us.js
+++ src/front/js/pages/about-us.js
@@ -8,7 +8,56 @@ export default function AboutUs() {
 
 	return (
 		<div className="container-fluid text-white">
-			<h1>About Us Page</h1>
+			<div>
+				<div className="container">
+					<h1 className="display-4">
+						{" "}
+						<span>Quienes Somos</span>{" "}
+					</h1>
+				</div>
+			</div>
+
+			<div className="row workingspace">
+				<div className="col-lg-6">
+					<img
+						src="https://www.trecebits.com/wp-content/uploads/2020/06/Como-crear-un-c%C3%B3digo-QR-800x445.jpg"
+						alt="Working Space"></img>
+				</div>
+				<div className="col-lg-5">
+					<h2>
+						El <span>Código QR</span> Que facilita <span>las cosas</span>
+					</h2>
+					<p>
+						Un código QR es un código de barras bidimensional cuadrada que puede almacenar los datos
+						codificados. Hoy en día, los códigos QR se pueden ver en folletos, carteles, revistas, etc.
+						Usted puede detectar fácilmente estos códigos de barras de dos dimensiones a tu alrededor.
+						Nuestra empresa le proporciona el código de barras con la información que usted necesite. Solo
+						debe registrarse, completar la información y el código se creara, si así de sencillo
+					</p>
+				</div>
+			</div>
+
+			<section className="testimonial">
+				<div className="row justify-content-center">
+					<div className="col-lg-8">
+						<p>Somos una empresa que busca facilitar la comunicacion a traves de codigos QR</p>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-lg-6 justify-content-center d-flex">
+						<img
+							src="https://cdn5.vectorstock.com/i/1000x1000/18/69/blue-qr-code-scanning-icon-or-design-logo-vector-18861869.jpg"
+							alt="Testimonial 2"
+							className="img-main"></img>
+					</div>
+				</div>
+				<div className="row justify-content-center info-text">
+					<div className="col-lg text-center">
+						<h5>QR +</h5>
+						<p>4Geeks Academy</p>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 }

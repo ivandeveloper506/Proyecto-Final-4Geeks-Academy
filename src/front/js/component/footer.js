09ab65d8cf0aa5dd4df0694d24941e3d3ea3c366
jairosantamaria <jairosantamaria@amelearning.net>
2021-04-24 18:32:17 +0000

    rev01

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import "../../styles/footer";
+import Image from "react-bootstrap/Image";
+import "../../styles/footer.scss";
 
 export const Footer = () => (
 	<div className="container-fluid main-footer-class">
 		<footer>
-			<div className="">
+			<div className="row">
 				<div className="footer-left col-md-4 col-sm-6">
 					<p className="about">
 						<span> About the company</span> Ut congue augue non tellus bibendum, in varius tellus
@@ -13,31 +14,39 @@ export const Footer = () => (
 						Suspendisse tellus elit, pharetra in hendrerit ut, aliquam quis augue. Nam ut nibh mollis,
 						tristique ante sed, viverra massa.
 					</p>
-					<div className="footer-center col-md-4 col-sm-6">
-						<div>
-							<i className="fa fa-map-marker" />
-							<p>
-								<span> Street name and number</span> City, Country
-							</p>
-						</div>
-						<div>
-							<i className="fa fa-phone" />
-							<p> (+00) 0000 000 000</p>
-						</div>
-						<div>
-							<i className="fa fa-envelope" />
-							<p>
-								<a href="#"> office@company.com</a>
-							</p>
-						</div>
-						<div className="footer-right col-md-4 col-sm-6">
-							<h2>
-								{""}
-								Company
-								<span> logo</span>
-							</h2>
-							<p className="name"> Company Name &copy; 2016</p>
-						</div>
+				</div>
+				<div className="footer-center col-md-4 col-sm-6">
+					<div>
+						<i className="fa fa-map-marker" />
+						<p>
+							<span> Torre Mercedes</span> San José, Costa Rica,
+						</p>
+					</div>
+					<div>
+						<i className="fa fa-phone" />
+						<p> (+506) 2590 2424</p>
+					</div>
+					<div>
+						<i className="fa fa-envelope" />
+						<p>
+							<a hrefclassName="text-primary"> servicioalcliente@qrservice.com</a>
+						</p>
+					</div>
+				</div>
+				<div className="footer-right col-md-4 col-sm-6">
+					<h2>
+						{""}
+						QR+Service
+					</h2>
+					<p className="name"> QR+Service &copy; 2021</p>
+					<div>
+						<img
+							src="https://www.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/core_market_full/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"
+							className="img-fluid"
+							width="100px"
+							height="100px"
+							alt=""
+						/>
 					</div>
 				</div>
 			</div>

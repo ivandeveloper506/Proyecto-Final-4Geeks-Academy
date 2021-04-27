8085a6c90faf9ae885ac64bc20e34e2d99c59b18
LilyBoza <lily96boza@gmail.com>
2021-04-27 02:44:29 +0000

    actualizacion

--- src/front/js/pages/services.js
+++ src/front/js/pages/services.js
@@ -10,41 +10,56 @@ export default function Services() {
 
 	return (
 		<div className="container-fluid text-white">
-			<h1>Services Page</h1>
 			<div className="container-fluid main-services-class">
 				<div className="row">
-					<p>
-						Qr+Service es una aplicación que te permite realizar un registro de tus datos personales,
-						padecimientos y medicamentos para tener un mayor control de tu historia medica o la de tus seres
-						queridos, a través del QR único generado para cada persona registrada.
-					</p>
+					<div className="col-md-6">
+						<div className="card principal-text-class">
+							<div className="card-body">
+								<p>
+									<strong>Qr+Service</strong> es una aplicación que te permite realizar un registro de
+									tus datos personales, padecimientos y medicamentos para tener un mayor control de tu
+									historia medica o la de tus seres queridos, a través del QR único generado para cada
+									persona registrada.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-6">col 2</div>
 				</div>
 				<h2>Ventajas de usar QR+Service</h2>
 				<div className="row">
-					<ul>
-						<li>
-							Puede ser utilizada por cualquier persona que desee tener un registro de sus datos en caso
-							de emergencia.
-						</li>
-						<li>
-							Es segura ya que solo usted podrá editar la información que irá en su QR o de las personas
-							que haya registrado a su nombre.
-						</li>
-						<li>
-							Muy útil para casos donde se deba cuidar de otras personas, con tan solo hacer la lectura
-							del código QR tener acceso a información medica o contacto de emergencia.
-						</li>
-					</ul>
-				</div>
-				<div className="row">
-					<div className="col-md-12">
-						<div>
-							<NavLink to="register">
-								<Button variant="btn btn-warning btn-lg">
-									Quiero registrarme <i className="fas fa-pen" />
-								</Button>
-							</NavLink>
+					<div className="col-md-6">col1</div>
+					<div className="col-md-6">
+						<div className="card second-text-class">
+							<div className="card-body">
+								<ul>
+									<li>
+										Puede ser utilizada por cualquier persona que desee tener un registro de sus
+										datos en caso de emergencia.
+									</li>
+									<li>
+										Es segura ya que solo usted podrá editar la información que irá en su QR o de
+										las personas que haya registrado a su nombre.
+									</li>
+									<li>
+										Muy útil para casos donde se deba cuidar de otras personas, con tan solo hacer
+										la lectura del código QR tener acceso a información medica o contacto de
+										emergencia.
+									</li>
+								</ul>
+							</div>
 						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-12">
+					<div className="PlaceButton">
+						<NavLink to="register">
+							<Button variant="btn btn-warning btn-lg">
+								Quiero registrarme <i className="fas fa-pen" />
+							</Button>
+						</NavLink>
 					</div>
 				</div>
 			</div>

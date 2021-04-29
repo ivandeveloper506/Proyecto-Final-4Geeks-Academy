dbb7b75fc0d6d4343e9f30a2c48d05841cb1db50
cbenavides1589 <cbenavides1589@gmail.com>
2021-04-29 06:09:17 +0000

    nuevos cambios de diseño

--- src/front/js/pages/about-us.js
+++ src/front/js/pages/about-us.js
@@ -61,6 +61,84 @@ export default function AboutUs() {
 					<h2 className="item-title">Explora los casos de uso del código QR en HD que te proporcionamos</h2>
 				</div>
 			</section>
+
+			<div className="timeline">
+				<div className="container left">
+					<div className="content">
+						<h2>2017</h2>
+						<p>Lorem ipsum..</p>
+					</div>
+				</div>
+				<div className="container right">
+					<div className="content">
+						<h2>2016</h2>
+						<p>Lorem ipsum..</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="timeline">
+				<div className="container left">
+					<div className="content">
+						<h2>2017</h2>
+						<p>
+							Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+							vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim
+							per, habeo iusto primis ea eam.
+						</p>
+					</div>
+				</div>
+				<div className="container right">
+					<div className="content">
+						<h2>2016</h2>
+						<p>
+							Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+							vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim
+							per, habeo iusto primis ea eam.
+						</p>
+					</div>
+				</div>
+				<div className="container left">
+					<div className="content">
+						<h2>2015</h2>
+						<p>
+							Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+							vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim
+							per, habeo iusto primis ea eam.
+						</p>
+					</div>
+				</div>
+				<div className="container right">
+					<div className="content">
+						<h2>2012</h2>
+						<p>
+							Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+							vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim
+							per, habeo iusto primis ea eam.
+						</p>
+					</div>
+				</div>
+				<div className="container left">
+					<div className="content">
+						<h2>2011</h2>
+						<p>
+							Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+							vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim
+							per, habeo iusto primis ea eam.
+						</p>
+					</div>
+				</div>
+				<div className="container right">
+					<div className="content">
+						<h2>2007</h2>
+						<p>
+							Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+							vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim
+							per, habeo iusto primis ea eam.
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }

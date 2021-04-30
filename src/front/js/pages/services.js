19e15e3c32753630e88fd26e61e971631bb534b8
ivandeveloper506 <ivanfonsecacr@gmail.com>
2021-04-30 22:48:28 +0000

    Mejoras al frontend

--- src/front/js/pages/services.js
+++ src/front/js/pages/services.js
@@ -5,9 +5,15 @@ import "../../styles/index.scss";
 import "../../styles/services.scss";
 import imgService1 from "../../img/img-service-1.png";
 import imgService2 from "../../img/img-service-2.png";
+import { QRCode } from "react-qrcode-logo";
+import { Table } from "react-bootstrap";
 
 export default function Conctat() {
 	const { store, actions } = useContext(Context);
+
+	const Like = () => {
+		return <i className="fas fa-thumbs-up" />;
+	};
 
 	return (
 		<div className="container-fluid main-div-class">
@@ -27,32 +33,97 @@ export default function Conctat() {
 						<div className="card-class">
 							<p>
 								<strong>Qr+Service</strong> es una aplicación que te permite registrar tus datos
-								personales, la lista de medicamentos y un registro de control de vacunas del COVID-19,
-								información que permitirá generar un Código QR para tener de forma digitalizada tan
-								importante información.
+								personales, la lista de medicamentos y un registro de control de vacunas. Información
+								que permitirá generar un Código QR para tener de forma digitalizada tan importante
+								información.
 							</p>
 						</div>
 					</div>
 				</div>
 
-				<div className="row mt-5">
-					<div className="col-md-6">
+				<div className="row mt-3">
+					<div className="col text-white">
+						<QRCode
+							value="QR+Service. ¡Registrese y sea parte de este gran beneficio!"
+							size="200"
+							ecLevel="H"
+							qrStyle="squares"
+							quietZone="20"
+							fgColor="#263238"
+							enableCORS="true"
+						/>
+					</div>
+				</div>
+
+				<div className="row mt-3">
+					<div className="col-md-7">
 						<div className="card-class">
-							<p>
-								Puede ser utilizada por cualquier persona que desee tener un registro de sus datos en
-								caso de emergencia.
+							<h2 className="p-3">¿Para que puedo usar QR+Services?</h2>
+
+							<Table variant="transparent">
+								<tbody>
+									<tr>
+										<td width="5">
+											<Like />
+										</td>
+										<td className="td-main-class">
+											Para cuando necesites cuidar de otras personas.
+										</td>
+									</tr>
+									<tr>
+										<td width="5">
+											<Like />
+										</td>
+										<td className="td-main-class">
+											Para disponer información importante generando un Código QR.
+										</td>
+									</tr>
+									<tr>
+										<td width="5">
+											<Like />
+										</td>
+										<td className="td-main-class">
+											Para registrar los medicamentos que usas diariamente.
+										</td>
+									</tr>
+									<tr>
+										<td width="5">
+											<Like />
+										</td>
+										<td className="td-main-class">Para llevar tu control de vacunas.</td>
+									</tr>
+									<tr>
+										<td width="5">
+											<Like />
+										</td>
+										<td className="td-main-class">
+											Para registrar información de contacto en caso de emergencia.
+										</td>
+									</tr>
+								</tbody>
+							</Table>
+
+							{/* <p>
+								<i className="fas fa-thumbs-up"></i> Para cuando necesites cuidar de otras personas.
 							</p>
 							<p>
-								Es segura ya que solo usted podrá editar la información que irá en su QR o de las
-								personas que haya registrado a su nombre.
+								<i className="fas fa-thumbs-up"></i> Para disponer tu información importante generando
+								un Código QR.
 							</p>
 							<p>
-								Muy útil para casos donde se deba cuidar de otras personas, con tan solo hacer la
-								lectura del código QR tener acceso a información medica o contacto de emergencia.
+								<i className="fas fa-thumbs-up"></i> Para registrar los medicamentos que usas
+								diariamente.
 							</p>
+							<p>
+								<i className="fas fa-thumbs-up"></i> Para llevar tu control de vacunas.
+							</p>
+							<p>
+								<i className="fas fa-thumbs-up"></i> Para registrar información de contacto en caso de
+								emergencia.
+							</p> */}
 						</div>
 					</div>
-					<div className="col-md-6 text-white">
+					<div className="col-md-5 text-white">
 						<div className="img-card-class">
 							<img src={imgService2} alt="Image Services 2" />
 						</div>
